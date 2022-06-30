ee97d6140c9fc0c078d46c35de928c3c0b16cf12
Mileta Cekovic <mcekovic@gmail.com>
2022-06-30 19:21:56 +0200

    More regions

--- src/main/java/org/strangeforest/ebird/domain/Region.java
+++ src/main/java/org/strangeforest/ebird/domain/Region.java
@@ -9,7 +9,19 @@ public enum Region {
    KOLUBARA("RS-09", "Kolubara"),
    PODUNAVLJE("RS-10", "Podunavlje"),
    BRANICEVO("RS-11", "Braničevo"),
-   SUMADIJA("RS-12", "Šumadija");
+   SUMADIJA("RS-12", "Šumadija"),
+   POMORAVLJE("RS-13", "Pomoravlje"),
+   BOR("RS-14", "Bor"),
+   ZAJECAR("RS-15", "Zaječar"),
+   ZLATIBOR("RS-16", "Zlatibor"),
+   MORAVICA("RS-17", "Moravica"),
+   RASKA("RS-18", "Raška"),
+   RASINA("RS-19", "Rasina"),
+   NISAVA("RS-20", "Nišava"),
+   TOPLICA("RS-21", "Toplica"),
+   PIROT("RS-22", "Pirot"),
+   JABLANICA("RS-23", "Jablanica"),
+   PCINJA("RS-24", "Pčinja");
 
    private final String code;
    private final String title;
